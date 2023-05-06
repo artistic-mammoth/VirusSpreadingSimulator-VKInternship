980e2bd18d52ef650d8649f1ab3d21c06e2da2d6
artistic_mammoth <thrl0ad@ya.ru>
2023-05-06 16:55:12 +0300

    Initial commit

---
+++ VirusSpreadingSimulator/VirusSpreadingSimulator/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirusSpreadingSimulator
+//
+//  Created by Михайлов Александр on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
